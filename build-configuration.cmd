fc7a32c1d57e90eb4437428226e96598b493f582
AlexPlom <alexiosplomaritis@gmail.com>
2018-09-05 12:09:16 +0300

    Second Initial Release

---
+++ build-configuration.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+@powershell -File .nyx\build.ps1 '--appname=Pandora.Git' '--nugetPackageName=Pandora.Git'
---
+++ src/Pandora.Git/Pandora.Git.rn.md
@@ -0,0 +1,5 @@
+#### 0.1.1 - 05.09.2018
+* Second Initial Release
+
+#### 0.1.0 - 05.09.2018
+* Initial Release
