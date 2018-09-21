343811ecdb2a8d87439c8de249ef1dfa308d818b
AlexPlom <alexiosplomaritis@gmail.com>
2018-09-21 17:34:54 +0300

    Updates pandora

---
+++ src/Pandora.Git/PandoraGitConfigurationSource.cs
@@ -0,0 +1,20 @@
+﻿using Elders.Pandora;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Pandora.Git
+{
+    public class PandoraGitConfigurationSource : IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            string applicationName = Environment.GetEnvironmentVariable("APPLICATION_NAME");
+            IPandoraContext context = new ClusterContext(applicationName);
+            IPandoraGitSettings gitSettings = new GitSettingsFromEnvironmentVariables();
+            IPandoraFactory pandoraFactory = new PandoraGitFactory(context, gitSettings);
+            Elders.Pandora.Pandora pandora = new Elders.Pandora.Pandora(pandoraFactory);
+
+            return new PandoraConfigurationProvider(pandora);
+        }
+    }
+}
